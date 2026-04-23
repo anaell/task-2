0e9ae580cca08a5050e2e43193ba4e86e28ba953
anaell <chimereanaele@gmail.com>
2026-04-23 08:58:24 +0100

    fix(module): fix module dependency issue Added the providers into the module feat(main.ts): enhance main application with global validation and error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseRepository } from './app.repository';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseRepository],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,62 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  BadRequestException,
+  UnprocessableEntityException,
+  ValidationPipe,
+} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // To enable cors
+  app.enableCors({ origin: '*' });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strips properties not in the DTO
+      // forbidNonWhitelisted: true, // Throws error if extra properties are sent
+      transform: true, // Auto-transforms payloads to DTO instances
+
+      exceptionFactory(errors) {
+        // Use a recursive search or just grab the first available constraint.
+        // I'm gonna grab just the first error.
+        const firstError = errors[0];
+
+        // If the error is nested, the constraints are inside 'children'
+        const constraints = firstError.constraints
+          ? firstError.constraints
+          : firstError.children?.[0].constraints;
+
+        // Take the very first error message found in the DTO
+        const message = Object.values(constraints!)[0];
+
+        // The below is to Check which constraint failed and then send an appropriate error response
+        // 'isString' corresponds to the @IsString decorator
+        if (
+          errors[0].constraints?.isString ||
+          errors[0].constraints?.isEnum ||
+          errors[0].constraints?.isNumber
+        ) {
+          return new UnprocessableEntityException({
+            status: 'error',
+            // The below was adjusted for task-2
+            // message: message,
+            message: 'Invalid query parameters',
+          });
+        }
+
+        // This return defines the EXACT JSON the user sees which follows what's recommended.
+        return new BadRequestException({ status: 'error', message: message });
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
+
+  // To get the full url after the app starts
+  const url = await app.getUrl();
+  console.log(`Application is running on: ${url}`);
 }
 bootstrap();
